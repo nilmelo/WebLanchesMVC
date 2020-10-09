63b10a232222ab3cb9f7e918d32b9248566e1b52
NilDevStudio <nildevstudio@outlook.com>
2020-10-09 16:03:20 -0300

    controle do carrinho

---
+++ Controllers/CartPurchaseController.cs
@@ -0,0 +1,57 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebLanchesMVC.Models;
+using WebLanchesMVC.Repositories;
+using WebLanchesMVC.ViewModel;
+
+namespace WebLanchesMVC.Controllers
+{
+    public class CartPurchaseController : Controller
+    {
+		private readonly ILunchRepository _lunchRepository;
+		private readonly CartPurchase _cartPurchase;
+
+		public CartPurchaseController(ILunchRepository lunchRepository, CartPurchase cartPurchase)
+		{
+			_lunchRepository = lunchRepository;
+			_cartPurchase = cartPurchase;
+		}
+
+		public IActionResult Index()
+		{
+			var items = _cartPurchase.GetCartPurchaseItems();
+			_cartPurchase.CartPurchaseItens = items;
+
+			var cartPurchaseViewModel = new CartPurchaseViewModel
+			{
+				CartPurchase = _cartPurchase,
+				CartPurchaseTotal = _cartPurchase.GetCartPurchaseTotal()
+			};
+
+			return View(cartPurchaseViewModel);
+		}
+
+		public RedirectToActionResult AddItemInCartPurchase(int lunchId)
+		{
+			var lunchSelected = _lunchRepository.Lunches.FirstOrDefault(p => p.Id == lunchId);
+
+			if(lunchSelected != null)
+			{
+				_cartPurchase.AddInCart(lunchSelected, 1);
+			}
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult RemoveItemFromCartPurchase(int lunchId)
+		{
+			var lunchSelected = _lunchRepository.Lunches.FirstOrDefault(p => p.Id == lunchId);
+
+			if(lunchSelected != null)
+			{
+				_cartPurchase.RemoveFromCart(lunchSelected);
+			}
+			return RedirectToAction("Index");
+		}
+
+    }
+}
---
+++ ViewModel/CartPurchaseViewModel.cs
@@ -0,0 +1,10 @@
+using WebLanchesMVC.Models;
+
+namespace WebLanchesMVC.ViewModel
+{
+    public class CartPurchaseViewModel
+    {
+        public CartPurchase CartPurchase { get; set; }
+		public decimal CartPurchaseTotal { get; set; }
+    }
+}
